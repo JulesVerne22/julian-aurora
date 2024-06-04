98333ff7572927e904b12acdb825d3e6d14b2255
Julian Smith <julian.smith@chameleoncg.com>
2024-06-04 11:01:44 -0500

    Added completed background and wezterm configuration

--- build.sh
+++ build.sh
@@ -15,11 +15,13 @@ curl --output-dir "/etc/yum.repos.d/" --remote-name https://copr.fedorainfraclou
 
 rpm-ostree install wezterm
 
+curl --output-dir "/etc/yum.repos.d/" --remote-name https://copr.fedorainfracloud.org/coprs/atim/lazygit/repo/fedora-40/atim-lazygit-fedora-40.repo
+
+rpm-ostree install lazygit
+
 rpm-ostree install neovim golang luarocks cargo ruby composer julia java-devel tree-sitter-cli fd-find
 
 echo "export WEZTERM_CONFIG_FILE=/usr/share/wezterm/wezterm.lua" >> /etc/profile
-
-npm install -g neovim
 
 # this installs a package from fedora repos
 rpm-ostree install screen
@@ -30,3 +32,7 @@ rpm-ostree install screen
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+
+ln -sf /usr/share/backgrounds/bluefin/WallPaper_SpringDay_Post.png /usr/share/backgrounds/default.png
+ln -sf /usr/share/backgrounds/bluefin/WallPaper_SummerNight_Post.png /usr/share/backgrounds/default-dark.png
+ln -sf /usr/share/backgrounds/bluefin/bluefin-spring-dynamic.xml /usr/share/backgrounds/default.xml
