d20c34f7939934f6b4a2271ded7a464c8d06e038
Julian Smith <68518708+JulesVerne22@users.noreply.github.com>
2024-06-04 09:31:46 -0500

    Update build.sh
    Removed wezterm config installation

--- build.sh
+++ build.sh
@@ -15,10 +15,6 @@ curl --output-dir "/etc/yum.repos.d/" --remote-name https://copr.fedorainfraclou
 
 rpm-ostree install wezterm
 
-mkdir /opt && cd /opt && mkdir wezterm && cd wezterm && git clone --depth 1 https://github.com/JulesVerne22/JulianSmith.wezterm.git . && git checkout ostree
-
-echo > "export WEZTERM_CONFIG_FILE=/opt/wezterm/wezterm.lua" >> /etc/profile
-
 rpm-ostree install neovim golang luarocks cargo ruby composer julia java-devel tree-sitter-cli fd-find
 
 mkdir /etc/xdg/nvim && cd /etc/xdg/nvim && git clone --depth 1 https://github.com/JulesVerne22/JulianSmith.nvim.git . && git checkout ostree
