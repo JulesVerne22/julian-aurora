7c7da2f5f10ea3b8442780220e55bdab2b2016d7
Julian Smith <julian.smith@chameleoncg.com>
2024-06-04 16:14:05 -0500

    Fixed background xmls and added wallpapers

---
+++ system_files/usr/share/plasma/look-and-feel/dev.getaurora.aurora.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
@@ -0,0 +1,4 @@
+applet.wallpaperPlugin = 'org.kde.image'
+applet.currentConfigGroup = ["Wallpaper", "org.kde.image", "General"]
+applet.writeConfig("Image", "/usr/share/wallpapers/WallPaper_SpringDay_Post.png")
+applet.reloadConfig()
